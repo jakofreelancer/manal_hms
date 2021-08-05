9096fe9f312bcd1927667b5802891fb7885c2457
jakofreelancer <58663181+jakofreelancer@users.noreply.github.com>
2021-08-05 14:52:50 +0800

    deleted configs

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,6 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCBbW-5QrL4J92sFPzCTpO2ZesLrIvISw0",
-    authDomain: "manal-bbf4e.firebaseapp.com",
-    databaseURL: "https://manal-bbf4e-default-rtdb.firebaseio.com",
-    projectId: "manal-bbf4e",
-    storageBucket: "manal-bbf4e.appspot.com",
-    messagingSenderId: "1034716497857",
-    appId: "1:1034716497857:web:a0d6d4f2e27faecb5b5140",
-    measurementId: "G-8V9LQ2BKLF"
 });
 
 const db = firebaseApp.firestore();
